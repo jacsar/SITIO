0c7b85db20eab7203a79d461f9734cb90ced3e65
jacsar <jacsar2009@gmail.com>
2021-11-08 20:12:02 -0600

    SITIO WEB SUBIDO

---
+++ for.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+var numero = prompt("Introduce un número y se mostrará su factorial");
+var resultado = 1;
+
+for(var i=1; i<=numero; i++) {
+  resultado *= i;
+}
+alert(resultado);
+</script>
